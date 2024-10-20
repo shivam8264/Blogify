686e869db9fa4dd8920d801dc165551056bc319b
shivam8264 <shivamsy05@gmail.com>
2024-10-20 12:49:52 +0530

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom"
+import { useSelector,useDispatch } from "react-redux"
+import { increaseCount,getCount } from "../features/posts/postsSlice"
+const Header = () => {
+    const dispatch=useDispatch();
+    const count=useSelector(getCount);
+    return (
+        <header className="Header">
+            <h1>Blogify Redux</h1>
+            <nav>
+                <ul>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="post">Post</Link></li>
+                    <li><Link to="user">Users</Link></li>
+                </ul>
+                <button onClick={()=>dispatch(increaseCount())}>{count}</button>
+            </nav>
+        </header>
+    )
+}
+
+export default Header
---
+++ src/features/users/UserPage.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { useSelector } from "react-redux"
+import { selectPostByUser} from "../posts/postsSlice"
+import { Link,useParams } from "react-router-dom"
+import { selectUserById } from './usersSlice'
+
+const UserPage = () => {
+    const {userId}=useParams();
+    const user=useSelector(state=>selectUserById(state,Number(userId)));
+    const postsForUser=useSelector(state=>selectPostByUser(state,Number(userId)));
+    const postTitles=postsForUser.map(post=>(
+        <li key={post.id}>
+            <Link to={`/post/${post.id}`}>{post.title}</Link>
+        </li>
+    ))
+  return (
+    <section>
+      <h2>{user?.name}</h2>
+      <ol>{postTitles}</ol>
+    </section>
+  )
+}
+
+export default UserPage
